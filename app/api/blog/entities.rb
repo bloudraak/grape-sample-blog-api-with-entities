e76839dba8789372880ef3599fa90491ca66e35e
Patrick Connolly <patrick@myplanetdigital.com>
2013-02-10 17:04:41 -0500

    Adds support for presenting numeric ids for blogs/posts/comments.

--- app/api/blog/entities.rb
+++ app/api/blog/entities.rb
@@ -6,16 +6,19 @@ module Blog
     class Weblog < Grape::Entity
       expose :title, :documentation => {:type => "string", :desc => "The title of the weblog"}
       expose :description, :documentation => {:type => "string", :desc => "A description of the weblog"}
+      expose :id, :documentation => {:type => "integer", :desc => "The numeric id of the weblog"}
     end
     
     class Post < Grape::Entity
       expose :title, :documentation => {:type => "string", :desc => "The title of the weblog"}
       expose :body, :documentation => {:type => "string", :desc => "The body of the post"}
+      expose :id, :documentation => {:type => "integer", :desc => "The numeric id of the post"}
     end
     
     class Comment < Grape::Entity
       expose :name, :documentation => {:type => "string", :desc => "The name of the commentor"}
       expose :body, :documentation => {:type => "string", :desc => "The body of the post"}
+      expose :id, :documentation => {:type => "integer", :desc => "The numeric id of the post"}
     end
   end
 end
